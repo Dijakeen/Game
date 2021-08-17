5524f160a619e00771e3bc73ae7cbe42ca1993f1
Kirill <dijakeen@gmail.com>
2021-08-17 17:52:07 +1000

    Add Sprites

--- Assets/fefefe.cs
+++ Assets/fefefe.cs
@@ -7,7 +7,7 @@ public class fefefe : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Пытаюсь скинуть изменения
     }
 
     // Update is called once per frame
